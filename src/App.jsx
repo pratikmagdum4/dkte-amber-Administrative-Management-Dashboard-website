bc05850589543bfb4da69eff6f4e596b4b664f3c
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-06-19 22:35:16 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import StudentSubmissionForm from './components/Home'
+import StudentSubmissionForm from './pages/Home'
 function App() {
   
 
