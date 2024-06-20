04dcbe15dd960fd3f693a5dc8e1003ee8a11dfea
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-06-20 11:50:05 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
+
 import './App.css';
-import LoginForm from './pages/Loginform';
+import LoginForm from './pages/LoginForm';
 import StudentSubmissionForm from './pages/Home';
 const App = () => {
 
