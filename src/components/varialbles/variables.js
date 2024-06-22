25e6e940aa0930552991add66644218664514df9
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-06-22 18:16:38 +0530

    add:admin folder

---
+++ src/components/varialbles/variables.js
@@ -0,0 +1,27 @@
+export const HomeLink = [
+  { label: "Home", url: "/" },
+  { label: "Login", url: "/login" },
+  { label: "Register", url: "/" },
+  { label: "Contact", url: "/" },
+];
+export const ClerkNavLink = [
+ 
+  { label: "clerk", url: "/" },
+  { label: "Login", url: "/login" },
+  { label: "Register", url: "/" },
+  { label: "Contact", url: "/" },
+];
+export const AdminNavLink = [
+  { label: "clerk", url: "/" },
+  { label: "Login", url: "/login" },
+  { label: "Register", url: "/" },
+  { label: "Contact", url: "/" },
+];
+
+export const ClerkLink = [
+  { label: "clerk", url: "/" },
+  { label: "Login", url: "/login" },
+  { label: "Register", url: "/" },
+  { label: "Contact", url: "/" },
+];
+
