50a2730497b4a07ed1cc97aedba964aa32972583
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-07-11 12:44:15 +0530

    add:axios table

--- src/pages/Clerk/FacultyAchivements/BooksPublication.jsx
+++ src/pages/Clerk/FacultyAchivements/BooksPublication.jsx
@@ -1,62 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AchievementsTable from '../../../components/ui/TableComponent';
-import axios from 'axios';
 import Navbar from '../../navbar/Navbar';
 import { FacultyAchivements } from '../../../components/varialbles/variables';
-const initialRows1 = [
-    { name: '', title: '', agency: '', isbnno: '' },
-];
-const initialRows2 = [
-    { name: '', title: '', agency: '', isbnno: '' ,chapter:''},
-];
-
-const columnHeaders1 = [
-    { key: 'name', label: 'Name of the Faculty' },
-    { key: 'title', label: 'Title of Book/Chapter' },
-    { key: 'agency', label: 'Publication Agency' },
-    { key: 'isbnno', label: 'ISBN Number' },
-    
-];
-
-const columnHeaders2 = [
-    { key: 'name', label: 'Name of the Faculty' },
-    { key: 'title', label: 'Title of Book/Chapter' },
-    { key: 'agency', label: 'Publication Agency' },
-    { key: 'isbnno', label: 'ISBN Number' },
-    { key: 'chapter', label: 'Chapter Number' },
-    // { key: 'date', label: 'Date' }, // Example additional column
-];
 
 const FacultyBooksPublication = () => {
+    const initialRows = [{ name: '', title: '', agency: '', isbnno: '', chapter: '' }];
 
-    const handleSubmit = async (data) => {
-        try {
-            const response = await axios.post('https://example.com/api/submit', data, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            console.log('Success:', response.data);
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+    const columnHeaders = [
+        { key: 'name', label: 'Name of the Faculty' },
+        { key: 'title', label: 'Title of Book/Chapter' },
+        { key: 'agency', label: 'Publication Agency' },
+        { key: 'isbnno', label: 'ISBN Number' },
+        { key: 'chapter', label: 'Chapter Number' },
+    ];
+
     return (
         <div>
-            <Navbar links={FacultyAchivements}/>
+            <Navbar links={FacultyAchivements} />
             <AchievementsTable
-                initialRows={initialRows1}
-                columnHeaders={columnHeaders1}
-                title="FACULTY ACHIEVEMENTS IN BOOKS PUBLICATION"
-                numberOfColumns={4} 
-                onSubmit={handleSubmit}
-            />
-            <AchievementsTable
-                initialRows={initialRows2}
-                columnHeaders={columnHeaders2}
+                initialRows={initialRows}
+                columnHeaders={columnHeaders}
                 title="FACULTY ACHIEVEMENTS IN BOOKS PUBLICATION"
                 numberOfColumns={5}
-                onSubmit={handleSubmit}
+                SubmitUrl="http://localhost:5000/api/facultyAchievements/submit"
+                FetchUrl="http://localhost:5000/api/facultyAchievements/getData"
             />
         </div>
     );
