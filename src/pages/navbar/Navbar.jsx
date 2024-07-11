0b4b6373fd0682302e288259a37b904af774afe4
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-07-11 21:29:04 +0530

    name

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import React from 'react'; const Navbar = ({links}) => {
         <nav className="bg-black text-white p-4 flex justify-between items-center">
             <div className="flex items-center">
                 <div className="bg-yellow-500 text-black font-bold px-2 py-1 mr-2">DKTE</div>
-                <span className="text-white">Ascendere</span>
+                <span className="text-white">Ambar</span>
             </div>
             <div className="flex space-x-4">
                 {links&&links.map((link, index) => (
