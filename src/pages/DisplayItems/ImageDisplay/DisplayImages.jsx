ab84efbe79f742eb1b840b56963d87b4d76eb1d1
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-08-05 23:13:59 +0530

    fix:display responsive

--- src/pages/DisplayItems/ImageDisplay/DisplayImages.jsx
+++ src/pages/DisplayItems/ImageDisplay/DisplayImages.jsx
@@ -7,7 +7,7 @@ import { BASE_URL } from '../../../api';
 const ImageDisplayList = () => {
     const [imageList, setImageList] = useState([]);
     const [imageType, setImageType] = useState('all');
-
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
     useEffect(() => {
         const fetchImages = async () => {
             try {
@@ -26,6 +26,15 @@ const ImageDisplayList = () => {
 
         fetchImages();
     }, [imageType]);
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsSmallScreen(window.innerWidth <= 640);
+        };
+        checkScreenSize();
+        window.addEventListener("resize", checkScreenSize);
+        return () => window.removeEventListener("resize", checkScreenSize);
+    }, []);
 
     return (
         <>
@@ -51,44 +60,83 @@ const ImageDisplayList = () => {
                     <div className="space-y-6">
                         {imageList.map((image) => (
                             <div key={image._id} className="p-4 border rounded-md shadow-sm grid grid-cols-1 md:grid-cols-3 gap-4">
-                                <div className="col-span-1 flex justify-center items-center">
-                                    <img
-                                        src={`${image.selfImage}`}
-                                        alt="Self"
-                                        className="w-32 h-32 object-cover rounded-full md:w-48 md:h-48"
-                                    />
-                                   
-                                </div>
-                                <div className="col-span-2">
-                                    
-                                    <h3 className="text-xl font-bold">{image.title}</h3>
-                                    <p className="text-gray-600">By {image.stdname}</p>
-                                    <p className="text-gray-600">Email: {image.email}</p>
-                                    <p className="text-gray-600">Contact: {image.contact}</p>
-                                    <p className="text-gray-600">Branch: {image.branch}</p>
-                                    <p className="text-gray-600">Year: {image.year}</p>
-                                    <p className="text-gray-600">Type: {image.imageType}</p>
-                                </div>
+                                {isSmallScreen ? (<> 
+                              
+                                    <div className="col-span-2">
+                                        <div className="col-span-1 flex justify-center items-center">
+                                            <img
+                                                src={`${image.selfImage}`}
+                                                alt="Self"
+                                                className="w-32 h-32 object-cover rounded-full md:w-48 md:h-48"
+                                            />
+
+                                        </div>
+                                        <h3 className="text-xl font-bold">{image.title}</h3>
+                                        <p className="text-gray-600">By {image.stdname}</p>
+                                        <p className="text-gray-600">Email: {image.email}</p>
+                                        <p className="text-gray-600">Contact: {image.contact}</p>
+                                        <p className="text-gray-600">Branch: {image.branch}</p>
+                                        <p className="text-gray-600">Year: {image.year}</p>
+                                        <p className="text-gray-600">Type: {image.imageType}</p>
+                                    </div>
+
+                                    <div className="col-span-3 mt-4 flex space-x-4">
+
+                                        <a
+                                            href={`${image.imageUrl}`}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="px-4 py-2 bg-blue-500 text-white rounded"
+                                        >
+                                            View
+                                        </a>
+                                        <a
+                                            href={`${image.imageUrl}`}
+                                            download={`${image.stdname},${image.branch}`}
+                                            target="_blank"
+                                            className="px-4 py-2 bg-green-500 text-white rounded"
+                                        >
+                                            Download
+                                        </a>
+                                    </div></>) : (<> <div className="col-span-1 flex justify-center items-center">
+                                        <img
+                                            src={`${image.selfImage}`}
+                                            alt="Self"
+                                            className="w-32 h-32 object-cover rounded-full md:w-48 md:h-48"
+                                        />
+
+                                    </div>
+                                        <div className="col-span-2">
+
+                                            <h3 className="text-xl font-bold">{image.title}</h3>
+                                            <p className="text-gray-600">By {image.stdname}</p>
+                                            <p className="text-gray-600">Email: {image.email}</p>
+                                            <p className="text-gray-600">Contact: {image.contact}</p>
+                                            <p className="text-gray-600">Branch: {image.branch}</p>
+                                            <p className="text-gray-600">Year: {image.year}</p>
+                                            <p className="text-gray-600">Type: {image.imageType}</p>
+                                        </div>
+
+                                        <div className="col-span-3 mt-4 flex space-x-4">
+
+                                            <a
+                                                href={`${image.imageUrl}`}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="px-4 py-2 bg-blue-500 text-white rounded"
+                                            >
+                                                View
+                                            </a>
+                                            <a
+                                                href={`${image.imageUrl}`}
+                                                download={`${image.stdname},${image.branch}`}
+                                                target="_blank"
+                                                className="px-4 py-2 bg-green-500 text-white rounded"
+                                            >
+                                                Download
+                                            </a>
+                                        </div></>)}
                                
-                                <div className="col-span-3 mt-4 flex space-x-4">
-                                   
-                                    <a
-                                        href={`${image.imageUrl}`}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="px-4 py-2 bg-blue-500 text-white rounded"
-                                    >
-                                        View
-                                    </a>
-                                    <a
-                                        href={`${image.imageUrl}`}
-                                        download={`${image.stdname},${image.branch}`}
-                                        target="_blank"
-                                        className="px-4 py-2 bg-green-500 text-white rounded"
-                                    >
-                                        Download
-                                    </a>
-                                </div>
                             </div>
                         ))}
                     </div>
--- src/pages/DisplayItems/LanguageSection/DisplayArticles.jsx
+++ src/pages/DisplayItems/LanguageSection/DisplayArticles.jsx
@@ -7,6 +7,7 @@ import { BASE_URL } from '../../../api';
 const ArticleDisplayList = () => {
     const [articleList, setArticleList] = useState([]);
     const [language, setLanguage] = useState('all');
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
     useEffect(() => {
         const fetchArticles = async () => {
@@ -27,6 +28,15 @@ const ArticleDisplayList = () => {
         fetchArticles();
     }, [language]);
 
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsSmallScreen(window.innerWidth <= 640);
+        };
+        checkScreenSize();
+        window.addEventListener("resize", checkScreenSize);
+        return () => window.removeEventListener("resize", checkScreenSize);
+    }, []);
+
     return (
         <>
             <Navbar links={AdminDisplayLink} />
@@ -44,7 +54,6 @@ const ArticleDisplayList = () => {
                         <option value="english">English</option>
                         <option value="hindi">Hindi</option>
                         <option value="marathi">Marathi</option>
-                        {/* Add more language options as needed */}
                     </select>
                 </div>
                 {articleList.length === 0 ? (
@@ -53,41 +62,84 @@ const ArticleDisplayList = () => {
                     <div className="space-y-6">
                         {articleList.map((article) => (
                             <div key={article._id} className="p-4 border rounded-md shadow-sm grid grid-cols-1 md:grid-cols-3 gap-4">
-                                <div className="col-span-1 flex justify-center items-center">
-                                    <img
-                                        src={`${article.selfImage}`}
-                                        alt="Self"
-                                        className="w-32 h-32 object-cover rounded-full md:w-48 md:h-48"
-                                    />
-                                </div>
-                                <div className="col-span-2">
-                                    <h3 className="text-xl font-bold">{article.title}</h3>
-                                    <p className="text-gray-600">By {article.stdname}</p>
-                                    <p className="text-gray-600">Email: {article.email}</p>
-                                    <p className="text-gray-600">Contact: {article.contact}</p>
-                                    <p className="text-gray-600">Branch: {article.branch}</p>
-                                    <p className="text-gray-600">Year: {article.year}</p>
-                                    <p className="text-gray-600">Language: {article.language}</p>
-                                </div>
-                                <div className="col-span-3 mt-4">
-                                    <div className="flex space-x-4 mt-2">
-                                        <a
-                                            href={article.content}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="px-4 py-2 bg-blue-500 text-white rounded"
-                                        >
-                                            View
-                                        </a>
-                                        <a
-                                            href={article.content}
-                                            download
-                                            className="px-4 py-2 bg-green-500 text-white rounded"
-                                        >
-                                            Download
-                                        </a>
-                                    </div>
-                                </div>
+                                {isSmallScreen ? (
+                                    <>
+                                       
+                                        <div className="col-span-2">
+                                            <div className="col-span-1 flex justify-center items-center">
+                                                <img
+                                                    src={article.selfImage}
+                                                    alt="Self"
+                                                    className="w-32 h-32 object-cover rounded-full"
+                                                />
+                                            </div>
+                                            <h3 className="text-xl font-bold">{article.title}</h3>
+                                            <p className="text-gray-600">By {article.stdname}</p>
+                                            <p className="text-gray-600">Email: {article.email}</p>
+                                            <p className="text-gray-600">Contact: {article.contact}</p>
+                                            <p className="text-gray-600">Branch: {article.branch}</p>
+                                            <p className="text-gray-600">Year: {article.year}</p>
+                                            <p className="text-gray-600">Language: {article.language}</p>
+                                        </div>
+                                        <div className="col-span-3 mt-4">
+                                            <div className="flex space-x-4 mt-2">
+                                                <a
+                                                    href={article.content}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="px-4 py-2 bg-blue-500 text-white rounded"
+                                                >
+                                                    View
+                                                </a>
+                                                <a
+                                                    href={article.content}
+                                                    download
+                                                    className="px-4 py-2 bg-green-500 text-white rounded"
+                                                >
+                                                    Download
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <div className="col-span-1 flex justify-center items-center md:justify-start">
+                                            <img
+                                                src={article.selfImage}
+                                                alt="Self"
+                                                className="w-32 h-32 object-cover rounded-full md:w-48 md:h-48"
+                                            />
+                                        </div>
+                                        <div className="col-span-2">
+                                            <h3 className="text-xl font-bold">{article.title}</h3>
+                                            <p className="text-gray-600">By {article.stdname}</p>
+                                            <p className="text-gray-600">Email: {article.email}</p>
+                                            <p className="text-gray-600">Contact: {article.contact}</p>
+                                            <p className="text-gray-600">Branch: {article.branch}</p>
+                                            <p className="text-gray-600">Year: {article.year}</p>
+                                            <p className="text-gray-600">Language: {article.language}</p>
+                                        </div>
+                                        <div className="col-span-3 mt-4">
+                                            <div className="flex space-x-4 mt-2">
+                                                <a
+                                                    href={article.content}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="px-4 py-2 bg-blue-500 text-white rounded"
+                                                >
+                                                    View
+                                                </a>
+                                                <a
+                                                    href={article.content}
+                                                    download
+                                                    className="px-4 py-2 bg-green-500 text-white rounded"
+                                                >
+                                                    Download
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </>
+                                )}
                             </div>
                         ))}
                     </div>
